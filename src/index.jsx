28a49ea99124c4191c69a49a9fde6fe007cb351c
Anthony <anthony.s.quispe@gmail.com>
2025-01-05 21:30:18 -0500

    Updated index Browser Router router to have a basename to fix redirection issue for github pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
-import App from "./App";
 import React from "react";
 import ReactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/PricingPage-Bootstrap">
       <App />
     </BrowserRouter>
   </React.StrictMode>
